a5ff2d0c0f1030a9eac26dd05027cff49a707fb7
momo-sti <momoka1112sti@gmail.com>
2023-06-30 15:46:06 +0900

    extrasページの見た目整える #10

---
+++ app/javascript/extra_bottom.js
@@ -0,0 +1,35 @@
+const bottomSheet = document.getElementById('bottom-sheet');
+
+// 開く
+function openBottomSheet() {
+  bottomSheet.style.bottom = '0';
+}
+
+// 閉じる
+function closeBottomSheet() {
+  bottomSheet.style.bottom = '-15%'; // 15%だけ見えるようにする
+}
+
+// フリック操作に対応する
+let start = null;
+bottomSheet.addEventListener('touchstart', e => {
+  start = e.touches[0].clientY;
+}, false);
+
+bottomSheet.addEventListener('touchend', e => {
+  const end = e.changedTouches[0].clientY;
+  // 上にフリックした場合
+  if (start - end > 50) {
+    openBottomSheet();
+  }
+  // 下にフリックした場合
+  else if (end - start > 50) {
+    closeBottomSheet();
+  }
+  start = null;
+}, false);
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  closeBottomSheet(); // ページが読み込まれたときにボトムシートを閉じる
+});
+
