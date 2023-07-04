58c66868f2c529e4318bf4af407f8c23cf7edb27
momo-sti <momoka1112sti@gmail.com>
2023-07-05 00:49:42 +0900

    Fix:各ファイルのjavascriptのincludeタグを削除して重複を解消

--- app/javascript/extra_button.js
+++ app/javascript/extra_button.js
@@ -1,14 +1,18 @@
 var editToggleButton = document.getElementById('edit-toggle-button');
+console.log('s')
 if (editToggleButton !== null) {
   editToggleButton.addEventListener('click', function() {
+    console.log('shown')
     var actionButtons = document.querySelectorAll('.action-buttons');
     actionButtons.forEach(function(button) {
       var parentElement = button.parentElement;
       if (button.classList.contains('hidden')) {
+        console.log('nya')
         button.classList.remove('hidden');
         button.classList.add('shown');
         parentElement.classList.add('shown');
       } else {
+        console.log('grr')
         button.classList.remove('shown');
         button.classList.add('hidden');
         parentElement.classList.remove('shown');
