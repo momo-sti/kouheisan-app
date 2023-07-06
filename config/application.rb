16bd18e1bc3bd78dd2c13f1df4a16ecd02bed191
momo-sti <momoka1112sti@gmail.com>
2023-07-06 15:26:59 +0900

    Add:エラーメッセージとgem rails-i18nの導入

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ Bundler.require(*Rails.groups)
 
 module Kouheisan
   class Application < Rails::Application
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.default_locale = :ja
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
