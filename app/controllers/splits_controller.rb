d9199415e86b96a92cb6b998cefa49680f44559a
momo-sti <momoka1112sti@gmail.com>
2023-07-07 19:54:02 +0900

    Fix:extraの更新を非同期に変更

--- app/controllers/splits_controller.rb
+++ app/controllers/splits_controller.rb
@@ -1,15 +1,9 @@
 class SplitsController < ApplicationController
-  def create
-    session[:number_of_people] = params[:number_of_people].to_i
-    update_split
+  before_action :calculate_total_amount, only: [:create, :update_split]
 
-    redirect_to splits_path
+  def create
   end
 
   def update_split
-    total_amount = session[:total_amount].to_f
-    number_of_people = session[:number_of_people]
-    split_amount = total_amount / number_of_people
-    session[:split_amount] = split_amount
   end
 end
