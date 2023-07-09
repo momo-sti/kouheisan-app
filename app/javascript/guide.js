69e52e6f2229c765b146f93a0b53fa5426ea531b
momo-sti <momoka1112sti@gmail.com>
2023-07-10 01:28:30 +0900

    Add:使い方ページ表示

--- app/javascript/guide.js
+++ app/javascript/guide.js
@@ -1,8 +1,12 @@
-document.addEventListener('turbo:load', function() {
+document.addEventListener("turbo:load", function() {
   const tg = new tourguide.TourGuideClient({
     autoScroll: true
   });
-  document.querySelector('#start').addEventListener('click', () => {
-    tg.start()  
-  });
+  
+  const startElement = document.querySelector("#start");
+  if (startElement) {  // 要素が存在するかどうかチェック
+    startElement.addEventListener("click", () => {
+      tg.start();
+    });
+  }
 });
