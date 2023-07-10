3dc38a40195b3bfab98a6e26629fc3a5b328dfc4
momo-sti <momoka1112sti@gmail.com>
2023-07-10 22:23:34 +0900

    Add:gem rack-cors導入

---
+++ config/initializers/cors.rb
@@ -0,0 +1,11 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'localhost:3000'
+            'kouheisan-app.fly.dev/'
+            'kouheisan.com/'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
