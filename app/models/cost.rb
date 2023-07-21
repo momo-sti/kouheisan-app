b030e8209f91e29111e4caf6aa8d31c9bd38af7d
momo-sti <momoka1112sti@gmail.com>
2023-07-21 11:18:08 +0900

    Add:o親モデルにdependent: :destroy追加

--- app/models/cost.rb
+++ app/models/cost.rb
@@ -1,5 +1,5 @@
 class Cost < ApplicationRecord
-  has_many :extra_costs, class_name: 'ExtraCost', foreign_key: 'cost_id'
+  has_many :extra_costs, dependent: :destroy, class_name: 'ExtraCost', foreign_key: 'cost_id'
   belongs_to :user
   accepts_nested_attributes_for :extra_costs
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_one_attached :avatar
-  has_many :costs
+  has_many :costs, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
