e6cdc9c5e35c0553cad7351a5182c718851d81ae
momo-sti <momoka1112sti@gmail.com>
2023-07-27 23:11:12 +0900

    Fix:toast_controller.jsに条件分岐追加

--- app/javascript/controllers/toast_controller.js
+++ app/javascript/controllers/toast_controller.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
   const toastElement = document.querySelector('#toast')
-  toastElement.style.visibility = "visible";
+  if (toastElement) {
+    toastElement.style.visibility = "visible";
 
-  setTimeout(function() {
-    toastElement.style.transform = "translateX(-100%)";
-  }, 2000);
+    setTimeout(function() {
+      toastElement.style.transform = "translateX(-100%)";
+    }, 2000);
+  }
 })
