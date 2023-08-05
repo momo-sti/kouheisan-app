32e45ba87c8eba365b9ab29ea109705ee63dd2cc
momo-sti <momoka1112sti@gmail.com>
2023-08-05 11:47:09 +0900

    costs_controllerの保存処理をcostモデルに切り出す #40

--- app/controllers/costs_controller.rb
+++ app/controllers/costs_controller.rb
@@ -1,9 +1,17 @@
 class CostsController < ApplicationController
   def create
-    is_paid = params[:action_type] == 'before' ? false : true
-    @cost = create_cost(is_paid)
-    save_extras if @cost.save
-    save_cost
+    is_paid = params[:action_type] != 'before'
+    params = build_cost_params(is_paid)
+    @cost = Cost.create_with_extras(params)
+
+    # @costががデータベースに保存されているかどうかを判定
+    if @cost.persisted?
+      # 処理が成功した場合
+      handle_success
+    else
+      # 失敗した場合
+      handle_failure
+    end
   end
 
   def save_per_person_cost
@@ -13,49 +21,38 @@ class CostsController < ApplicationController
 
   private
 
-  def create_cost(is_paid)
-    gasoline_cost = session[:result].presence || 0
-    Cost.new(
+  def build_cost_params(is_paid)
+    {
       user_id: current_user.id,
       total_amount: session[:total_amount] || 0,
       per_person_cost: session[:per_person_cost] || 0,
-      gasoline_cost:,
+      gasoline_cost: session[:result].presence || 0,
       distance: session['gasoline']['total_distance'] || 0,
       fuel_efficiency: session['gasoline']['fuel_efficiency'] || 0,
       price_per_liter: session['gasoline']['price_per_liter'] || 0,
       highway_cost: session[:highway_cost] || 0,
       start_place: session[:start_place].present? ? session[:start_place] : 'なし',
       arrive_place: session[:arrive_place].present? ? session[:arrive_place] : 'なし',
-      is_paid:
-    )
+      is_paid:,
+      extras: session[:extras]
+    }
   end
 
-  def save_extras
-    return unless session[:extras]
-
-    session[:extras].each do |extra|
-      @cost.extra_costs.create(
-        category: extra['category'],
-        amount: extra['amount']
-      )
+  def handle_success
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to callback_path }
     end
   end
 
-  def save_cost
-    if @cost.persisted?
-      respond_to do |format|
-        format.turbo_stream
-        format.html { redirect_to callback_path }
+  def handle_failure
+    puts @cost.errors.full_messages
+    flash[:alert] = @cost.errors.full_messages.join(', ')
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace('error_message', partial: 'error_message', locals: { message: flash[:alert] })
       end
-    else
-      puts @cost.errors.full_messages
-      flash[:alert] = @cost.errors.full_messages.join(', ')
-      respond_to do |format|
-        format.turbo_stream do
-          render turbo_stream: turbo_stream.replace('error_message', partial: 'error_message', locals: { message: flash[:alert] })
-        end
-        format.html { redirect_to callback_path }
-      end
+      format.html { redirect_to callback_path }
     end
   end
 
--- app/models/cost.rb
+++ app/models/cost.rb
@@ -1,5 +1,20 @@
 class Cost < ApplicationRecord
   has_many :extra_costs, dependent: :destroy, class_name: 'ExtraCost', foreign_key: 'cost_id'
   belongs_to :user
-  accepts_nested_attributes_for :extra_costs
+  accepts_nested_attributes_for :extra_costs # ExtraCost オブジェクトも同時に保存
+
+  def self.create_with_extras(params)
+    # extras以外のparamsを初期化
+    cost = new(params.except(:extras))
+    ActiveRecord::Base.transaction do
+      cost.save!
+      params[:extras]&.each do |extra|
+        cost.extra_costs.create!(
+          category: extra['category'],
+          amount: extra['amount']
+        )
+      end
+    end
+    cost
+  end
 end
