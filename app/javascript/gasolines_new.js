69f22c47e644b96df3229b0fd64654bd7fff95b0
momo-sti <momoka1112sti@gmail.com>
2023-09-22 11:27:10 +0900

    Add:ガソリン計算画面の距離フィールドに初期値設定

--- app/javascript/gasolines_new.js
+++ app/javascript/gasolines_new.js
@@ -6,10 +6,23 @@ document.addEventListener('turbo:load', function() {
   // HTMLに表示
   var distanceElement = document.getElementById('distance');
   var durationElement = document.getElementById('duration');
+  
   if (distanceElement) {
     distanceElement.textContent = km;
   }
   if (durationElement) {
     durationElement.textContent = time;
+  }
+
+  // "km" を取り除く（もし含まれていれば）
+  if (km && typeof km === 'string') {
+    km = km.replace(' km', '');
+  }
+
+  // total_distanceのinputフィールドに値を設定
+  var totalDistanceInput = document.querySelector("input[name='gasoline[total_distance]']");
+  
+  if (totalDistanceInput && km) {
+    totalDistanceInput.value = km;
   }
 });
